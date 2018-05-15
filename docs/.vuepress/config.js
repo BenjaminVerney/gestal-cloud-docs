afaad309dc9544748c8c6025852e34e01260e209
gcoguiec <gcoguiec@jygatech.com>
2018-05-14 20:45:05 -0400

    Add favicons and manifest, as well as service workers.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,20 @@ module.exports = {
   title: 'Gestal Cloud API',
   description: 'Learn how to build an integration, send data for synchronization or interact with farm sites using our Web API.',
   dest: 'dist',
+  serviceWorker: true,
+  head: [
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#0e335f' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#0e335f' }]
+  ],
   themeConfig: {
     logo: '/logo.svg',
     docsDir: 'docs',
+    repo: 'jyga',
     lastUpdated: 'Last Updated',
     nav: [
       { text: 'Home', link: '/' },
