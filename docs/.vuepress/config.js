4ea3254bfecef326c4d31da687eadca55058d638
gcoguiec <gcoguiec@jygatech.com>
2018-05-16 17:36:20 -0400

    Add portal to developer

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Developer',
+  title: 'Developer Portal',
   description: 'Learn how to build an integration, send data for synchronization or interact with farm sites using our Web API.',
   dest: 'dist',
   serviceWorker: true,
